1a863780d46afb47d24f2f41766d66c07f26ac58
袁龙 <yuanlong@kuaigeng.com>
2018-01-23 15:39:46 +0800

    add

---
+++ GlobalFile/GLobalDelegate.h
@@ -0,0 +1,15 @@
+//
+//  GLobalDelegate.h
+//  OCGlobal
+//
+//  Created by Eddie on 2018/1/23.
+//  Copyright © 2018年 yl. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@protocol GLobalDelegate <NSObject>
+
+- (void)scaleBgView;
+
+@end
